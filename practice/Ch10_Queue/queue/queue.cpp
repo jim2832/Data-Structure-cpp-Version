756f2b49f1ecea50ab826fd0cf1ea671b433c769
Jim <jim880705@gmail.com>
2023-02-02 23:41:06 +0800

    230202 update

---
+++ practice/Ch10_Queue/queue/queue.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+struct Node{
+    T Data;
+    Node* Next;
+};
+
+template <typename T>
+class Queue{
+    private:
+        Node<T>* First;
+        Node<T>* Rear;
+
+    public:
+        Queue(); //建構式
+        T Front(); //回傳刪除端的資料
+        T Back(); //回傳新增端的資料
+        bool Empty(); //檢查是否為空
+        int Size(); //回傳queue長度
+        void Push(T); //將資料從queue尾端新增
+        void Pop(); //將資料從queue前端刪除
+        void Print_Queue(); //印出queue中的所有資料
+};
+
+//建構式
+template <typename T>
+Queue<T>::Queue(){
+    First = Rear = nullptr;
+}
+
+//回傳刪除端的資料
+template <typename T>
+T Queue<T>::Front(){
+    if(Empty()){
+        cout << "This queue is empty" << endl;
+        return 0;
+    }
+    return First->Data;
+}
+
+//回傳新增端的資料
+template <typename T>
+T Queue<T>::Back(){
+    if(Empty()){
+        cout << "This queue is empty" << endl;
+        return 0;
+    }
+    return Rear->Data;
+}
+
+//檢查是否為空
+template <typename T>
+bool Queue<T>::Empty(){
+    return (First == nullptr);
+}
+
+//回傳queue長度
+template <typename T>
+int Queue<T>::Size(){
+    if(Empty()){
+        cout << "This queue is empty" << endl;
+        return 0;
+    }
+    int len = 1;
+    Node<T>* current = First;
+    while(current != Rear){
+        len++;
+        current = current->Next;
+    }
+    return len;
+}
+
+//將資料從queue尾端新增
+template <typename T>
+void Queue<T>::Push(T value){
+    if(Empty()){
+        First = Rear = new Node<T>;
+        First->Data = value;
+        Rear->Data = value;
+        First->Next = nullptr;
+        Rear->Next = nullptr;
+        return;
+    }
+
+    Node<T>* new_node = new Node<T>;
+    new_node->Data = value;
+    new_node->Next = nullptr;
+    Rear->Next = new_node;
+    Rear = new_node;
+}
+
+//將資料從queue前端刪除
+template <typename T>
+void Queue<T>::Pop(){
+    if(Empty()) return;
+
+    Node<T>* temp = First;
+    First = First->Next;
+    delete temp;
+    if(First == nullptr){
+        Rear = nullptr;
+    }
+}
+
+//印出queue中的所有資料
+template <typename T>
+void Queue<T>::Print_Queue(){
+    if(Empty()){
+        cout << "This queue is empty" << endl;
+    }
+    else{
+        Node<T>* current = First;
+        cout << "Queue: ";
+        while(current != Rear){
+            cout << current->Data << " ";
+            current = current->Next;
+        }
+        cout << current->Data << endl; //印出rear
+    }
+}
+
+int main(void){
+    Queue<int> data;
+
+    for(int i=1; i<=10; i++){
+        data.Push(i);
+    }
+
+    data.Print_Queue();
+
+    cout << "Front: " << data.Front() << endl;
+    cout << "Back: " << data.Back() << endl;
+
+    data.Pop();
+    data.Print_Queue();
+    data.Push(11);
+    data.Print_Queue();
+
+    cout << "Front: " << data.Front() << endl;
+    cout << "Back: " << data.Back() << endl;
+
+    return 0;
+}
