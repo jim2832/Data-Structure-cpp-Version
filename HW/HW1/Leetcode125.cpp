73eecc6b0af8e537c06e16398d02da9620d22a5a
Jim <jim880705@gmail.com>
2023-02-21 23:23:20 +0800

    230221 update

---
+++ HW/HW1/Leetcode125.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        bool isPalindrome(string s){
+            int len = s.length();
+            bool is_Alphanumeric = false;
+            for(int i=0; i<len; i++){
+                //判斷是否為數字或字母
+                if(!isalnum(s[i])){
+                    s[i] = ' ';
+                }
+                s[i] = tolower(s[i]); //轉為小寫
+            }
+            s.erase(std::remove(s.begin(), s.end(), ' '), s.end()); //將空格消除
+            len = s.length();
+
+            if(s.length() == 1){
+                return true;
+            }
+
+            //判斷是否為回文
+            for(int i=0; i<len/2; i++){
+                if(s[i] != s[len-i-1]){
+                    return false;
+                }
+            }
+            return true;
+        }
+};
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+    // string a = "1100";
+    // int index = 0;
+    // cout << "a[" << index << "] = " << a[index] - '0';
+    cout << 1/5;
+
+    return 0;
+}
